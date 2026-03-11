f5b6d3eb044e8500963b8442ddc7ba5cad459e11
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-03-11 11:55:42 -0300

    style: add Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/sections/Header";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Clima",
@@ -16,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`h-screen w-screen md:bg-[url('/assets/bg.svg')] bg-[url('/assets/bg-mobile.png')] bg-cover bg-center`}>
+    <html lang="pt-BR" className={`${poppins.variable}`}>
+      <body
+        className={`h-screen w-screen md:bg-[url('/assets/bg.svg')] bg-[url('/assets/bg-mobile.png')] bg-cover bg-center`}
+      >
         <Header />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
