e62f0e157f3d3e78fbacf735f7a8845d8b0dddab
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-03-11 11:49:55 -0300

    style: update input component styles

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,8 +2,8 @@ export default function Input() {
     return (
         <input
             type="text"
-            placeholder="Search here..."
-            className="text-foreground text-2xl border-none bg-(--surface) w-full h-full py-2.5 px-7.5 rounded-xl"
+            placeholder="Search Here"
+            className="placeholder:text-(--foreground) placeholder:font-extralight text-2xl border-none bg-(--surface) w-full md:max-w-89 h-21 px-7.5 rounded-xl"
         />
     )
 }
