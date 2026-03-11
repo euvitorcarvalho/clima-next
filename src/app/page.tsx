b8a9d94e057b0402e858adc92c710e216e65551e
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-03-11 11:59:13 -0300

    fix: fix filter import, and add filter component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Main from "@/components/layout/Main";
 import { Intro } from "../components/layout/Intro";
 import Input from "@/components/ui/Input";
+import Filters from "@/components/ui/Filters";
 
 export default function Home() {
   return (
     <div>
+      {/* fazer background no menu da header versão mobile */}
+      {/* tentar fazer a responsividade da barra de filtro e search por conta própria */}
       <Intro />
       <Main>
-        <div className="flex justify-between flex-wrap items-center gap-5">
+        <div className="flex justify-between sm:flex-nowrap flex-wrap items-center sm:gap-45 gap-5 py-18.5">
           <Input></Input>
+          <Filters></Filters>
         </div>
       </Main>
     </div>
