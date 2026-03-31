d07ab62ed4d13d35d10878f22186757957b15579
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-03-31 00:15:35 -0300

    feat: create const of weather conditions for icons

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,6 +9,28 @@ interface FilterOptions {
   value: string;
 }
 
+interface WeatherCondition {
+  type: string;
+  img: string;
+}
+
+interface WeatherConditions {
+  [key: string]: WeatherCondition;
+}
+
+export const conditions: WeatherConditions = {
+  clear: { type: "sunny", img: "sunny" },
+  clouds: { type: "cloudy", img: "cloudy" },
+  rain: { type: "rainy", img: "rainy" },
+  drizzle: { type: "rainy", img: "rainy" },
+  thunderstorm: { type: "rainy", img: "rainy" },
+  snow: { type: "rainy", img: "rainy" },
+  mist: { type: "cloudy", img: "cloudy" },
+  smoke: { type: "cloudy", img: "cloudy" },
+  haze: { type: "cloudy", img: "cloudy" },
+  fog: { type: "cloudy", img: "cloudy" },
+};
+
 export const temperatureFilter: FilterOptions[] = [
   {
     name: "Temperature",
