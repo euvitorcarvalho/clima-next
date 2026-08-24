00b8ce819f663525cd4387e319460499f671278f
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-08-24 07:52:30 -0300

    feat: cria lógica para armazenar valor do input de busca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import Main from "@/components/layout/Main";
 import { Intro } from "../components/layout/Intro";
 import Input from "@/components/ui/Input";
 import Filters from "@/components/ui/Filters";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
+
   return (
     <div>
-      {/* fazer background no menu da header versão mobile */}
-      {/* tentar fazer a responsividade da barra de filtro e search*/}
       <Intro />
       <Main>
         <div className="flex sm:flex-row flex-col flex-wrap lg:justify-between justify-center items-center gap-5 lg:py-18.75 py-8">
-          <Input></Input>
+          <Input value={inputValue} valueChanged={setInputValue}></Input>
           <Filters></Filters>
         </div>
       </Main>
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,9 +1,16 @@
-export default function Input() {
-    return (
-        <input
-            type="text"
-            placeholder="Search Here"
-            className="placeholder:text-(--foreground) placeholder:font-extralight sm:text-2xl text-xl border-none bg-(--surface) w-full md:max-w-89 sm:h-21 h-12 sm:px-7.5 px-3 rounded-xl"
-        />
-    )
+interface SearchInputProps {
+  value: string;
+  valueChanged: (texto: string) => void;
+}
+
+export default function Input({ value, valueChanged }: SearchInputProps) {
+  return (
+    <input
+      type="text"
+      placeholder="Search Here"
+      value={value}
+      onChange={(event) => valueChanged(event.target.value)}
+      className="placeholder:text-(--foreground) placeholder:font-extralight sm:text-2xl text-xl border-none bg-(--surface) w-full md:max-w-89 sm:h-21 h-12 sm:px-7.5 px-3 rounded-xl"
+    />
+  );
 }
