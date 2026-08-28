0c02df60e5ac5db08adf54882bb8f2847d3b92b0
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2026-08-28 13:10:01 -0300

    feat: recria rota de geocoding de cabeça para fins de aprendizado

--- src/app/api/geocoding/route.ts
+++ src/app/api/geocoding/route.ts
@@ -1,51 +1,41 @@
 import { NextResponse } from "next/server";
-// route.js
-export async function GET(request: Request) {
-  // try e catch
-  try {
-    // pego o nome da localização na url
-    const { searchParams } = new URL(request.url);
-    const location = searchParams.get("location");
 
-    //retorna erro se não houver localização
-    if (!location) {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const location = searchParams.get("location");
+
+  if (!location) {
+    return NextResponse.json(
+      { error: "Informe uma localização" },
+      { status: 400 },
+    );
+  }
+
+  const apiKey = process.env.WEATHER_API_KEY;
+  const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${6}&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(geocodingURL);
+    const data = await response.json();
+
+    if (data.length === 0) {
       return NextResponse.json(
-        { error: "Informe uma localização" },
+        { error: `Nenhuma localidade encontrada para: ${location}` },
         { status: 400 },
       );
     }
 
-    // pega a chave da api no .env e coloca na url do geocodig
-    const apiKey = process.env.WEATHER_API_KEY;
-    const geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${6}&appid=${apiKey}`;
-
-    // fetch padrão
-    const response = await fetch(geocodingUrl);
-    const data = await response.json();
-
-    // transforma o json data padrão em uma string para chamar a api de clima "Salvador,BR", por exemplo
-    interface locationsType {
+    interface LocationsType {
       name: string;
       country: string;
     }
 
-    const locations = data.map((location: locationsType) => {
+    let locations = data.map((location: LocationsType) => {
       return `${location.name},${location.country}`;
     });
 
-    // retorna erro se nenhuma localização com aquele nome for encontrada
-    if (data.length === 0) {
-      return NextResponse.json(
-        { error: "Nenhuma localização encontrada." },
-        { status: 400 },
-      );
-    }
-
-    // se sucesso, retorna os dados
-    return NextResponse.json({ data: locations }, { status: 200 });
-
-    // o catch retorna os erros de conexão com a api
+    return NextResponse.json({ locations: locations }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "Falha de conexão." }, { status: 500 });
+    return NextResponse.json({ error: "Falha na conexão." }, { status: 500 });
   }
 }
